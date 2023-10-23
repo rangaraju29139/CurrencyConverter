5b0c0849369aec120e1c0723b9cdb965b8fc50d6
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-10-23 20:33:41 +0530

    added the eureka server and loadbalancing using the feign

--- src/main/java/com/example/currencyconverter/CurrencyExchangeProxy.java
+++ src/main/java/com/example/currencyconverter/CurrencyExchangeProxy.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange",url = "http://localhost:8000")
+//@FeignClient(name="currency-exchange",url = "http://localhost:8000")
+@FeignClient(name="currency-exchange")  // inorder to do the load balancing by calling the name server for the list of currency exchange services
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
